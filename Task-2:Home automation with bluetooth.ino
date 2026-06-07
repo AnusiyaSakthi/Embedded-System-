83d27fa709e5733d6e691f2a16683e1d28a4b48d
AnusiyaSakthi <anusiyasakthi4@gmail.com>
2026-06-07 20:10:18 +0530

    Create Task-2:Home automation with bluetooth.ino

---
+++ Task-2:Home automation with bluetooth.ino
@@ -0,0 +1,14 @@
+#Smart home automation system 
+Objective:
+To design and simulate a Bluetooth-based home automation system using arduino for controlling multiple home appliances
+
+Components Used:
+  Arduino Uno
+  Breadboard
+  4 LEDs
+    LED 1 - Light
+    LED 2 - Fan
+    LED 3 - TV
+    LED 4 - Water pump
+  4*220ohm resistors
+  
